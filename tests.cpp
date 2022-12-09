d78fa1c26595738da8d190ca1e88bbca89237cc3
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-12-08 20:36:33 -0500

    Create tests.cpp

---
+++ tests.cpp
@@ -0,0 +1,30 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "main.cpp"
+#include <vector>
+
+TEST_CLASS(Vector)
+
+  TEST_METHOD(TextMethod1)
+{
+int n = 1;
+}
+
+TEST_METHOD(TextMethod2)
+{
+int n = 2;
+}
+
+TEST_METHOD(TextMethod3)
+{
+int n = 3;
+}
+
+TEST_METHOD(TextMethod4)
+{
+int n = 4;
+}
+TEST_METHOD(TextMethod5)
+{
+int n = 5;
+}
